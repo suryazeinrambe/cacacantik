fe6a7934e82ad66b53016f36a32778ed1cfbdd6a
Surya Zein Rambe <suryazein2002@gmail.com>
2024-09-18 22:32:13 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+
+var typed = new Typed(".text", {
+  strings: ["Salsanabila Anggraeni",'Kang PUNDUNGGGG 🥰😘😍😡😡🤬'],
+  smartBackSpace: true,
+  typeSpeed: 100,
+  loop: true,
+});
